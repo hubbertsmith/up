fe3af74549ce0c906f761e10d37ad785f707757c
hubbertsmith <52464327+hubbertsmith@users.noreply.github.com>
2020-01-10 15:22:25 -0700

    plots
    plots

--- ggplot code.R
+++ ggplot code.R
@@ -10,11 +10,11 @@ summary(anes_subset02$age)
 
 
 anes_subset02$age <- "NA"
-anes_subset02$age[anes_subset02$V161267>= 18 | anes_subset02$V161267<= 29] <- "18-30 years old"
-anes_subset02$age[anes_subset02$V161267>= 30 | anes_subset02$V161267<= 39] <- "30-39 years old"
-anes_subset02$age[anes_subset02$V161267>= 40 | anes_subset02$V161267<= 49] <- "40-49 years old"
-anes_subset02$age[anes_subset02$V161267>= 50 | anes_subset02$V161267<= 59] <- "50-59 years old"
-anes_subset02$age[anes_subset02$V161267>= 60] <- "60+ years old"
+anes_subset02$age[anes_subset02$V161267>= 18 | anes_subset02$V161267<= 29] <- "18-30"
+anes_subset02$age[anes_subset02$V161267>= 30 | anes_subset02$V161267<= 39] <- "30-39"
+anes_subset02$age[anes_subset02$V161267>= 40 | anes_subset02$V161267<= 49] <- "40-49"
+anes_subset02$age[anes_subset02$V161267>= 50 | anes_subset02$V161267<= 59] <- "50-59"
+anes_subset02$age[anes_subset02$V161267>= 60] <- "60+"
 anes_subset02$age <- factor(anes_subset02$age)
 
 summary(anes_subset02$age)
